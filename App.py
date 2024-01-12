40592e513fa6c0ce9f5abff1575b5df13ca078f8
Prathamesh Dhas <126050846+iamprathameshdhas@users.noreply.github.com>
2024-01-12 08:53:00 +0530

    App.py

--- App.py
+++ App.py
@@ -63,41 +63,7 @@ if uploaded_file is not None:
     image = np.asarray(Image.open(uploaded_file).resize((256,246)))
     
     
-    
 
-# Additional content or analysis can be added based on your specific requirements
-# For example, you can include a button for processing the image and providing results.
-# feature-descriptor-1: Hu Moments
-def fd_hu_moments(image):
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    feature = cv2.HuMoments(cv2.moments(image)).flatten()
-    return feature
-# feature-descriptor-2: Haralick Texture
-def fd_haralick(image):
-    # convert the image to grayscale
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    # compute the haralick texture feature vector
-    haralick = mahotas.features.haralick(gray).mean(axis=0)
-    # return the result
-    return haralick
-# feature-descriptor-3: Color Histogram
-def fd_histogram(image, mask=None,bins=256):
-    # convert the image to HSV color-space
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
-    # compute the color histogram
-    hist  = cv2.calcHist([image], [0, 1, 2], None, [bins, bins, bins], [0, 256, 0, 256, 0, 256])
-    # normalize the histogram
-    cv2.normalize(hist, hist)
-    # return the histogram
-    return hist.flatten()
-def get_hog(image, _wind_size=8, _orientations=8):
-    
-    """generate Histogram of gradients as a feature vector for an given image """
-    
-    fd, hog_image = hog(image, orientations=_orientations, pixels_per_cell=(_wind_size, _wind_size),
-                    cells_per_block=(1, 1), visualize=True, channel_axis=-1, feature_vector=True)
-    
-    return fd
 # Sample button for further processing (modify as needed)
 import random
 if st.button("Process Image"):
